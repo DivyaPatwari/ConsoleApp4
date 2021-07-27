8d93c422d36a855d537ce0b78a4a7f5d5581a636
DivyaPatwari <divyapatwari09@gmail.com>
2021-07-27 17:43:32 +0530

    class added now

---
+++ ConsoleApp4/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Class1
+    {
+        public static void main()
+        {
+            Console.WriteLine("this is class 1");
+        }
+    }
+}
