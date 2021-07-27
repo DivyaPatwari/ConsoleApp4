7893ba359cae5fd0995e5372eb9e9a04ec5a4626
DivyaPatwari <54354529+DivyaPatwari@users.noreply.github.com>
2021-07-27 17:25:23 +0530

    Update Program.cs

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -8,7 +8,10 @@ namespace ConsoleApp4
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("1");
-            Console.WriteLine("here we have understood how to push and pull c# files to git");
+            Console.WriteLine("here we have understood how to push  c# files to git");
+            
+            
+            Console.WriteLine("this is done to pull file from git ");
         }
     }
 
